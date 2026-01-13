e710c39f7194cfdcac751b7c5d7c59282ab393c1
adarsh-html15 <mauryadarsh15@gmail.com>
2026-01-13 21:54:48 +0000

    datatype , conversion and operations

---
+++ javascript/datatype_conversion_operation.js
@@ -0,0 +1,19 @@
+let age = 21
+let name = "Adarsh"
+console.table({ name, age });
+console.log(typeof age); 
+// number conversion to string
+let stringAge = String(age)
+// display the changes
+console.log(typeof stringAge);
+console.log(stringAge);
+let changeName = Number(name)
+console.log(changeName);
+console.log(typeof changeName);
+
+console.log();
+console.log('2' + 2); 
+console.log(2 + '2');
+console.log('2' + 2 + 2);
+console.log(2 + 2 + '2');
+console.log();
