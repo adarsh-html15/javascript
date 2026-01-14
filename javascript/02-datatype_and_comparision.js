466d216608d04a9ed35e2684407df6b372d78d4f
adarsh-html15 <mauryadarsh15@gmail.com>
2026-01-14 10:09:42 +0000

    change

---
+++ javascript/02-datatype_and_comparision.js
@@ -0,0 +1,30 @@
+// Comparison Operators
+console.log(2 < 2)
+console.log(2 <= 2)
+console.log(2 > 2)
+console.log(2 >= 2)
+console.log(2 == '2')
+console.log(2 === '2')
+console.log(2 != '2')
+console.log(2 !== '2')
+
+// data types in JavaScript
+// Primitive Data Types
+// Number,boolean,string,undefined,null,symbol,bigint
+
+// Non-Primitive Data Types
+// Object,Array,Function
+let objest = {
+    name: "adarsh",
+    age: 22,
+    city: "delhi"
+
+}
+ 
+let myFunction = function () {
+    console.log("Adarsh code")
+}
+let array = [1, 2, 3, 4, 5, 6, 7][0, 9, 8, 7, 6, 5, 4]
+console.log(typeof objest)
+console.log(typeof myFunction)
+console.log(typeof array)
