d5bf38dfc077919c69edd834ac1bd5b08f3e8f57
adarsh-html15 <mauryadarsh15@gmail.com>
2026-01-16 18:18:13 +0000

    num and maths

--- javascript/06-num_and_math.js
+++ javascript/06-num_and_math.js
@@ -1,8 +1,25 @@
-const balence = 2222
+const balance = 2222
 const price = new Number(3333)
 
-console.log(balence);
-console.log(typeof balence);
-
+console.log(balance);
+console.log(typeof balance);
 console.log(price);
 console.log(typeof price);
+
+console.log(balance.toString().length);
+console.log(typeof balance);
+
+
+console.log(price.toString().length);
+
+
+const a = 10.123456789
+console.log(a.toFixed(2));
+console.log(a.toFixed(5));
+console.log(a.toFixed(8));
+// now there are lots of method in the math object
+console.log(Math.PI);
+console.log(Math.round(4.6));
+console.log(Math.floor(4.9));
+console.log(Math.ceil(4.1));
+console.log(Math.trunc(4.9));
