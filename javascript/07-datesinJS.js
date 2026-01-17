edc420e355a7b01800311c91eb82ec9a2b4a1ab3
adarsh-html15 <mauryadarsh15@gmail.com>
2026-01-17 18:31:20 +0000

    date

--- javascript/07-datesinJS.js
+++ javascript/07-datesinJS.js
@@ -9,4 +9,6 @@ let mynewdate = new Date("2034,2,3 4:5:6:7")
 log(mynewdate.toString())
 log(mynewdate.toLocaleString())
 
-
+let newdate = Date.now()
+console.log(newdate)
+console.log(newdate.toString());
