e21b9ea8e750e776296a32d405d074fecf43972b
adarsh-html15 <mauryadarsh15@gmail.com>
2026-01-23 04:02:00 +0000

    change

---
+++ 02-basic/01-array.js
@@ -0,0 +1,13 @@
+const myArray = [10, 20, 30, 40, 50];
+console.log("First element:", myArray[0]);
+
+console.log("Third element:", myArray[2]);
+console.log(myArray.length)
+ 
+const newArray = new Array(5, 4, 634, 5654, 63);
+console.log("New array:", newArray);
+
+console.log("Length of new array:", newArray.length);
+
+newArray[2] = 999;
+console.log("Modified new array:", newArray);
