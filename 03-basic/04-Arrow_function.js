12c9bea4be24d416c6223c14a6bdfbba5f1f494c
adarsh-html15 <mauryadarsh15@gmail.com>
2026-03-09 19:52:04 +0000

    arrow function

---
+++ 03-basic/04-Arrow_function.js
@@ -0,0 +1,41 @@
+const user = {
+    name: "Adarsh",
+    price: 100,
+     
+    welcomeMassage: function() {
+        console.log(`Welcome, ${this.name}! Your price is ${this.price}`);
+       // this refers to the user object current contaxet
+       console.log(this);
+    }
+               
+};
+console.log(this); // global context
+// this in global context refers to the global object (window in browsers, global in Node.js)
+user.welcomeMassage()
+user.name = "Adarsh Kumar";
+user.welcomeMassage()
+
+
+// function chai() {
+//     console.log(this.username); 
+// }
+// chai(); 
+
+// arrow method to creat a function 
+const chai = () => {
+    console.log(this.username); 
+}
+chai();
+
+const arrowFunction = (num1 ,num2) => { 
+    return num1 + num2;
+    // explicite return
+}
+console.log(arrowFunction(5, 10));
+
+// implicit return
+const impliciteReturn = (num1, num2) => num1 * num2;
+// no return needed for single expression, it will return the result of that expression
+console.log(impliciteReturn(5, 10));
+
+                                          
