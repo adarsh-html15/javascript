82de94cee2b6c823382bf3dc7d392afe84f616ef
adarsh-html15 <mauryadarsh15@gmail.com>
2026-03-30 19:06:30 +0000

    for loop

--- 05-iteration/for_loop.js
+++ 05-iteration/for_loop.js
@@ -2,7 +2,7 @@
 
 for (let i = 0; i < 10; i++) {
     const element = i;
-    if (i==5) {
+    if (i == 5) {
         console.log("5 is the best number");
     }
     console.log(element);
@@ -12,9 +12,18 @@ for (let i = 0; i < 10; i++) {
 for (let i = 0; i < 10; i++) {
     const element = i;
     console.log(`ouuter loop: ${element}`);
-    for (let j = 0; j <10; j++) {
+    for (let j = 0; j < 10; j++) {
         const element2 = j;
         console.log(`inner loop: ${element2} while outer loop is ${element}`);
+        for (let k = 0; k < 10; k++) {
+            const element3 = k;
+            console.log(`innermost loop: ${element3} while outer loop is ${element} and middle loop is ${element2}`);
+            for (let l = 0; l < 10; l++) {
+                const element4 = l;
+                console.log(`deepest loop: ${element4} while outer loop is ${element} and middle loop is ${element2} and inner loop is ${element3}`);
+            }
+        }
+
     }
 }
 
