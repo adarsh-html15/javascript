ff99256797f8a87006685e9fb703bc97e24c7958
Adarsh maurya <mauryadarsh15@gmail.com>
2026-08-31 15:45:35 +0000

    new prototype

--- 11-oops/oops.js
+++ 11-oops/oops.js
@@ -1,3 +1,16 @@
 const user = {
-
+    username:"Adarsh",
+    loginCount: 9,
+    signedIn: true,
+    getUserDetails:function () {
+        console.log("got user details from the database");
+        console.log(this);
+        
+        console.log(`Username is ${this.username}`)
+    }
 }
+
+console.log(user.username);
+console.log(user.getUserDetails());
+
+
