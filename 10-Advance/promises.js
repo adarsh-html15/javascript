979d948b621e6e718d1f1701a07747681bdb9550
Adarsh maurya <mauryadarsh15@gmail.com>
2026-08-31 14:39:52 +0000

    commit

--- 10-Advance/promises.js
+++ 10-Advance/promises.js
@@ -1,46 +1,46 @@
-const promiseOne = new Promise(function(resolve, reject){
+const promiseOne = new Promise(function (resolve, reject) {
     // do an async task
     //  DB calls, cryptography, network
     setTimeout(() => {
         console.log('Async task is complated');
         resolve()
-    },1000);
+    }, 1000);
 })
 
-promiseOne.then(function(){
+promiseOne.then(function () {
     console.log("Promise cosumed")
 })
 
-new Promise(function(resolve, reject){
+new Promise(function (resolve, reject) {
     setTimeout(() => {
         console.log("Asinc task 2");
         resolve()
-        
+
     }, 1000);
-}).then(function(){
+}).then(function () {
     console.log("async 2 resolved");
-    
+
 });
 
 
-const promiseThree = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        resolve({username: "Adarsh", email:"Adarsh@gmail.com"})
-    },1000)
+const promiseThree = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve({ username: "Adarsh", email: "Adarsh@gmail.com" })
+    }, 1000)
 });
-promiseThree.then(function(user){
- console.log(user);
- 
+promiseThree.then(function (user) {
+    console.log(user);
+
 })
 
 
-const promiseFour = new promise(function(resolve,reject){
+const promiseFour = new Promise(function (resolve, reject) {
     setTimeout(() => {
         let error = true
         if (!error) {
-        resolve({username:"Adarsh",password:"123"})            
-        }else{
-           reject('somethingis wrong') 
+            resolve({ username: "Adarsh", password: "123" })
+        } else {
+            reject('somethingis wrong')
         }
     }, 1000);
 })
@@ -48,11 +48,43 @@ const promiseFour = new promise(function(resolve,reject){
 promiseFour.then((user) => {
     console.log(user);
     return user.username
-    
-}).then((username)=>{
-     console.log(username);
-     
-}).catch((error)=>{
+
+}).then((username) => {
+    console.log(username);
+
+}).catch((error) => {
     console.log(error);
-    
+
+}).finally(() => console.log("promise eighter resolve or error"));
+
+
+const promiseFive = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let error = true
+        if (!error) {
+            resolve({ username: "Aakansha", password: "123" })
+        } else {
+            reject('some are wrong')
+        }
+    }, 1000);
 })
+
+async function consumePromiseFive() {
+    const response = await promiseFive
+    console.log(response);
+
+}
+
+consumePromiseFive()
+
+async function getAllUsers() {
+    try {
+        const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+        const data = await response.json()     //to get the data in json
+        console.log(data);
+    } catch (error) {
+        console.log("E:",error)
+    }
+}
+getAllUsers()
+
